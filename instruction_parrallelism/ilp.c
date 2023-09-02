8e23d87effadc962ad25a6e7901a16af19a32160
Danilo Vladicic <danilo.vladicic@gmail.com>
2023-09-02 16:07:04 -0700

    ilp

---
+++ instruction_parrallelism/ilp.c
@@ -0,0 +1,12 @@
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = a + b;
+    int f = b + c;
+    int e = a + d;
+    
+    return 0;
+}
+
+main();
---
+++ instruction_parrallelism/no_ilp.c
@@ -0,0 +1,11 @@
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = a + b;
+    int e = a + d;
+    int f = b + c;
+    return 0;
+}
+
+main();
