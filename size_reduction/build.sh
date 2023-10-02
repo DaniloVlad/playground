ebfac86a8e459c9b3981b2b3949000825bbd62d8
Danilo Vladicic <danilo.vladicic@gmail.com>
2023-10-01 22:07:44 -0700

    final v

--- size_reduction/build.sh
+++ size_reduction/build.sh
@@ -4,8 +4,20 @@ make clean
 make all
 cp bin/base bin/base_stripped
 cp bin/small bin/small_stripped
-cp bin/base bin/base_stripped
+cp bin/base_os bin/base_os_stripped
+cp bin/base_static bin/base_static_stripped
 
-objdump -d bin/base > run/base
-objdump -d bin/small > run/small
-la -lah bin/
+
+#strip some stuffs:
+strip -S --strip-unneeded --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.eh_frame --remove-section=.eh_frame_ptr --remove-section=.eh_frame_hdr ./bin/*_stripped
+objdump -D bin/base > run/base
+objdump -D bin/base_os > run/base_os
+objdump -D bin/base_static > run/base_static
+objdump -D bin/small > run/small
+
+objdump -D bin/base_stripped > run/base_stripped
+objdump -D bin/base_os_stripped > run/base_os_stripped
+objdump -D bin/base_static_stripped > run/base_static_stripped
+objdump -D bin/small_stripped > run/small_stripped
+
+ls -lah bin/
